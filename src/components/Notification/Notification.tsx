c2bc6a8173d57d3f2bcb669bd897a83ca12172b9
Oleksii Ponomarenko <55384068+Oleksii996@users.noreply.github.com>
2026-02-19 17:16:00 +0100

    Initial commit

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,7 @@
+import css from "../Notification/Notification.module.css";
+
+const Notification = () => {
+  return <p className={css.message}>No feedback yet</p>;
+};
+
+export default Notification;
