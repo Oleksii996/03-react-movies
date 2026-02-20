c9d0d309f61f0fb2db221d156b972bbe6f0410fa
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-02-20 21:02:06 +0100

    update code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,51 +1,9 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "../CafeInfo/CafeInfo";
+import CafeInfo from "../Loader/Loader";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-import Notification from "../Notification/Notification";
+import Notification from "../MovieGrid/MovieGrid";
 import type { Votes } from "../../types/votes";
 
 type VoteType = keyof Votes;
-
-const App = () => {
-  const [votes, setVotes] = useState<Votes>({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  // Функція для обробки
-  const handleVote = (type: VoteType) => {
-    setVotes((prevVotes) => ({
-      ...prevVotes,
-      [type]: prevVotes[type] + 1,
-    }));
-  };
-
-  // Функція для скидання
-  const resetVotes = () => {
-    setVotes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
-  const totalVotes = votes.good + votes.neutral + votes.bad;
-  const canReset = totalVotes > 0;
-
-  return (
-    <div className={css.app}>
-      <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={canReset} />
-      {totalVotes > 0 ? (
-        <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={Math.round((votes.good / totalVotes) * 100)} />
-      ) : (
-        <Notification />
-      )}
-    </div>
-  );
-};
-
-export default App;
--- src/components/Notification/Notification.tsx
+++
@@ -1,7 +0,0 @@
-import css from "../Notification/Notification.module.css";
-
-const Notification = () => {
-  return <p className={css.message}>No feedback yet</p>;
-};
-
-export default Notification;
